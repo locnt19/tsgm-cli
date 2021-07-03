cda60c47fad1f08655d2ab11aafa55643139ce2f
locnt19 <locnt19@gmail.com>
2021-07-03 18:25:08 +0700

    🚀 build template

--- dist/utils/template.js
+++ dist/utils/template.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.render = void 0;
 var ejs = require("ejs");
-exports.render = function (content, data) {
+var render = function (content, data) {
     return ejs.render(content, data);
 };
+exports.render = render;
 //# sourceMappingURL=template.js.map
